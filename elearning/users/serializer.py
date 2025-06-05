462d73fac85177c34e7684fd7c175013227d0abc
adershash <adershashp17@gmail.com>
2025-06-05 13:09:17 +0530

    nested serialization is implemented

--- elearning/users/serializer.py
+++ elearning/users/serializer.py
@@ -1,7 +1,9 @@
 from rest_framework import serializers
 from .models import Users
+from courses.serializer import CourseSerializer
 
 class UserSerializer(serializers.ModelSerializer):
+    reg_courses=CourseSerializer(many=True,read_only=True)
     class Meta:
         model=Users
         fields='__all__'
