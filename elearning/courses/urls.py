aa351a96c5993cad3733f903817ea480bfd9e022
adershash <adershashp17@gmail.com>
2025-06-05 12:15:50 +0530

    the viewset is implemented for the course module

--- elearning/courses/urls.py
+++ elearning/courses/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from rest_framework import routers
+
+router=routers.DefaultRouter()
+router.register('courses',views.Course,basename='course')
 
 urlpatterns=[
-    path('courses/',views.Course.as_view()),
-    path('courses/<int:pk>/',views.CourseDetails.as_view())
+    # path('courses/',views.Course.as_view()),
+    # path('courses/<int:pk>/',views.CourseDetails.as_view())
+    path('',include(router.urls))
 ]
--- elearning/courses/views.py
+++ elearning/courses/views.py
@@ -1,24 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework import mixins,generics
 from .serializer import CourseSerializer
 from .models import Courses
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
-#mixins
-class Course(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+# #mixins
+# class Course(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+#     queryset=Courses.objects.all()
+#     serializer_class=CourseSerializer
+
+#     def get(self,request):
+#         return self.list(request)
+    
+#     def post(self,request):
+#         return self.create(request)
+    
+# #generics
+
+# class CourseDetails(generics.RetrieveUpdateDestroyAPIView):
+#     queryset=Courses.objects.all()
+#     serializer_class=CourseSerializer
+#     lookup_field= 'pk'
+
+
+#ViewSets example
+"""
+class Course(viewsets.ViewSet):
+    def list(self,request):
+        queryset=Courses.objects.all()
+        serializer=CourseSerializer(queryset,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+        
+    def retrieve(self,request,pk=None):
+        course=get_object_or_404(Courses,pk=pk)
+        serializer=CourseSerializer(course)
+        return Response(serializer.data)
+"""
+#ModelViewSets example
+
+class Course(viewsets.ModelViewSet):
     queryset=Courses.objects.all()
     serializer_class=CourseSerializer
-
-    def get(self,request):
-        return self.list(request)
-    
-    def post(self,request):
-        return self.create(request)
-    
-#generics
-
-class CourseDetails(generics.RetrieveUpdateDestroyAPIView):
-    queryset=Courses.objects.all()
-    serializer_class=CourseSerializer
-    lookup_field= 'pk'
